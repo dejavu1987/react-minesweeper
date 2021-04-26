614c4ee2110832b96c7ea3665c2cdfa82427fc7f
Anil Maharjan <anil@anilmaharjan.com.np>
2021-04-26 11:51:36 +0200

    Cell states

--- src/Minesweeper.jsx
+++ src/Minesweeper.jsx
@@ -1,18 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MinesweeperClass from "./lib/minesweeper";
+const CELL_OPEN = "open";
+const CELL_FLAGGED = "flagged";
 
 export default function Minesweeper({ level }) {
-  const minesweeper = new MinesweeperClass(level);
-  const [board, setBoard] = useState(minesweeper.filledBoard);
+  const [board, setBoard] = useState([]);
+  const [cellStates, setCellStates] = useState({});
+  const [totalMines, setTotalMines] = useState(0);
+
+  useEffect(() => {
+    const minesweeper = new MinesweeperClass(level);
+    setBoard(minesweeper.filledBoard);
+    setTotalMines(minesweeper.mines.length);
+    setCellStates([]);
+  }, [level]);
 
   return (
     <div className="minesweeper">
-      <h4>💣 {minesweeper.mines.length}</h4>
+      <h4>💣 {totalMines}</h4>
       <div className="board">
-        {board.map((row) => (
+        {board.map((row, y) => (
           <div className="row">
-            {row.map((cell) => (
-              <div className={`col color-${cell}`}>{cell}</div>
+            {row.map((cell, x) => (
+              <div
+                className={`col color-${cell} ${
+                  !cellStates[`${x},${y}`] ? "not-open" : ""
+                }`}
+                onClick={() =>
+                  setCellStates({
+                    ...cellStates,
+                    [`${x},${y}`]: CELL_OPEN
+                  })
+                }
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  setCellStates({
+                    ...cellStates,
+                    [`${x},${y}`]: CELL_FLAGGED
+                  });
+                }}
+              >
+                {cellStates[`${x},${y}`] &&
+                  (cellStates[`${x},${y}`] === CELL_OPEN ? cell : "🔒")}
+              </div>
             ))}
           </div>
         ))}
